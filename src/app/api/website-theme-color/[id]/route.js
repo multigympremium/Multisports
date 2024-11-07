84e4919c9c880339510b4923bc2e398c66ea2bb3
anamulamin <aadelbanat8991@gmail.com>
2024-11-07 12:42:32 +0600

    created website theme color backend

---
+++ src/app/api/website-theme-color/[id]/route.js
@@ -0,0 +1,114 @@
+import connectDB from "@/dbConfig/dbConfig";
+import { deleteFile, uploadFile } from "@/helpers/aws-s3";
+import WebsiteThemeColorModel from "@/models/WebsiteThemeColorModel/WebsiteThemeColorModel";
+import { NextResponse } from "next/server";
+
+connectDB();
+
+// GET Request: Get brand by ID
+export async function GET(req, { params }) {
+  const id = params?.id;
+  try {
+    const brand = await WebsiteThemeColorModel.findOne({ _id: id });
+
+    if (!brand) {
+      return NextResponse.json(
+        { success: false, message: "Brand not found" },
+        { status: 404 }
+      );
+    }
+    return NextResponse.json({ success: true, data: brand }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { success: false, error: error.message },
+      { status: 400 }
+    );
+  }
+}
+
+// PUT Request: Update brand by ID
+export async function PUT(req, { params }) {
+  const id = params?.id;
+  const formData = await req.formData();
+
+  const existingData = await WebsiteThemeColorModel.findById(id);
+  if (!existingData) {
+    return NextResponse.json(
+      { success: false, message: "Brand not found" },
+      { status: 404 }
+    );
+  }
+
+  const unitName = formData.get("unitName");
+
+  if (!unitName) {
+    return NextResponse.json(
+      { success: false, message: "Required fields missing" },
+      { status: 400 }
+    );
+  }
+
+  const submitData = {
+    unitName,
+  };
+
+  try {
+    const updatedBrand = await WebsiteThemeColorModel.findByIdAndUpdate(
+      id,
+      submitData,
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+
+    if (!updatedBrand) {
+      return NextResponse.json(
+        { success: false, message: "Brand not found" },
+        { status: 404 }
+      );
+    }
+    return NextResponse.json(
+      { success: true, data: updatedBrand },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { success: false, error: error.message },
+      { status: 400 }
+    );
+  }
+}
+
+// DELETE Request: Delete brand by ID
+export async function DELETE(req, { params }) {
+  const id = params?.id;
+  try {
+    const brandItem = await WebsiteThemeColorModel.findOne({ _id: id });
+
+    if (!brandItem) {
+      return NextResponse.json(
+        { success: false, message: "Brand not found" },
+        { status: 404 }
+      );
+    }
+
+    const deletedBrand = await WebsiteThemeColorModel.deleteOne({ _id: id });
+    if (!deletedBrand) {
+      return NextResponse.json(
+        { success: false, message: "Brand not found" },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json(
+      { success: true, message: "Brand deleted successfully" },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { success: false, error: error.message },
+      { status: 400 }
+    );
+  }
+}
