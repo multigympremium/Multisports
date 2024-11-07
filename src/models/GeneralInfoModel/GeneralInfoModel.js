2ead5fc15a517121040ec89355c752a8660c4fe5
anamulamin <aadelbanat8991@gmail.com>
2024-11-07 10:17:49 +0600

    created general in model

---
+++ src/models/GeneralInfoModel/GeneralInfoModel.js
@@ -0,0 +1,54 @@
+import mongoose from "mongoose";
+
+const productSizeSchema = new mongoose.Schema(
+  {
+    company_name: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    google_map_link: {
+      type: String,
+    },
+    play_store_link: {
+      type: String,
+    },
+    app_store_link: {
+      type: String,
+    },
+    trade_license: {
+      type: String,
+    },
+    tin_no: {
+      type: String,
+    },
+    min_no: {
+      type: String,
+    },
+    footer_copyright: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+const GeneralInfoModel =
+  mongoose.models.general_info ||
+  mongoose.model("general_info", productSizeSchema);
+
+export default GeneralInfoModel;
