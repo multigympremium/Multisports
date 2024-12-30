bd1f0a064dc30961ba5f316c8967e159a09779a9
anamulamin <aadelbanat8991@gmail.com>
2024-12-30 12:35:47 +0600

    included related product

--- src/Hook/useAxiosPublic.jsx
+++ src/Hook/useAxiosPublic.jsx
@@ -4,11 +4,9 @@ const axiosPublic = axios.create({
   // baseURL: backendUrl ? backendUrl : "https://king-prawn-app-qkhg8.ondigitalocean.app/api",
   baseURL: import.meta.env.VITE_BACKEND_URL + "/api",
   // timeout: 30000
-
 });
 
 const useAxiosPublic = () => {
-
   return axiosPublic;
 };
 
