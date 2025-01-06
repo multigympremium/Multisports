213d45416a33f0a6274a4a18292ea7546d18a1fe
Minhajul Islam <mmminhaj221@gmail.com>
2025-01-06 16:21:13 +0600

    completed product details page

--- src/components/partial/RelatedProducts/RelatedProducts.jsx
+++ src/components/partial/RelatedProducts/RelatedProducts.jsx
@@ -1,6 +1,10 @@
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import { FaArrowRight } from "react-icons/fa";
+import { useEffect, useRef, useState } from "react";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+
 import useAxiosPublic from "../../../Hook/useAxiosPublic";
 import ProductSkeleton from "../ProductCard/ProductSkeleton";
 import ProductCard from "../ProductCard/ProductCard";
@@ -52,6 +56,8 @@ const RelatedProducts = ({ limit = 4, isShowSeeAll = true, category }) => {
       .getElementById(`modal_${product.productTitle.replace(/\s+/g, "_")}`)
       .showModal();
   };
+  const prevRef = useRef(null);
+    const nextRef = useRef(null);
 
   return (
     <section className="w-[90%] md:w-full mx-auto py-6">
@@ -61,8 +67,8 @@ const RelatedProducts = ({ limit = 4, isShowSeeAll = true, category }) => {
       {loading ? (
         <ProductSkeleton skeletons={4} />
       ) : (
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 md:gap-6">
-          {products?.length > 0 &&
+        <div className="">
+          {/* {products?.length > 0 &&
             products
               .slice(0, limit)
               .map((product, index) => (
@@ -81,7 +87,65 @@ const RelatedProducts = ({ limit = 4, isShowSeeAll = true, category }) => {
                     handleProductClick={handleProductClick}
                   />
                 </div>
-              ))}
+              ))} */}
+
+
+          <div className="relative">
+            <Swiper
+              modules={[Navigation]}
+              navigation={{
+                prevEl: prevRef.current,
+                nextEl: nextRef.current,
+              }}
+              onSwiper={(swiper) => {
+                swiper.params.navigation.prevEl = prevRef.current;
+                swiper.params.navigation.nextEl = nextRef.current;
+                swiper.navigation.init();
+                swiper.navigation.update();
+              }}
+              spaceBetween={0}
+              slidesPerView={2}
+              breakpoints={{
+                768: { slidesPerView: 4 },
+                1024: { slidesPerView:4 },
+              }}
+              loop={true}
+            >
+              {products?.length > 0 &&
+                products.map((product, index) => (
+                  <SwiperSlide key={product.id}>
+                    <div className="px-2">
+                      <ProductCard
+                        product={product}
+                        showNewArrival={true}
+                        showDiscount={true}
+                        handleProductClick={handleProductClick}
+                      />
+                    </div>
+                  </SwiperSlide>
+                ))}
+            </Swiper>
+
+            {/* Custom Navigation Buttons */}
+            <button
+              ref={prevRef}
+              className="absolute top-1/3 -left-2 md:-left-9 transform -translate-y-1/2 w-7 h-7 md:w-11 md:h-11 flex items-center justify-center bg-white text-black rounded-full border-l transition-all duration-300 ease-in-out md:hover:bg-gray-700  hover:text-white hover:scale-110 hover:shadow-2xl z-10"
+            >
+              <IoIosArrowBack className="text-base" />
+            </button>
+
+            <button
+              ref={nextRef}
+              className="absolute top-1/3 -right-2 md:-right-9 transform -translate-y-1/2 w-7 h-7 md:w-11 md:h-11 flex items-center justify-center  bg-white text-black rounded-full border-r  transition-all duration-300 ease-in-out md:hover:bg-gray-700 hover:text-white hover:scale-110 hover:shadow-2xl z-10"
+            >
+              <IoIosArrowForward className="text-base" />
+            </button>
+          </div>
+
+
+
+
+
         </div>
       )}
     </section>
