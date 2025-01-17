7a07f8fa7a4be819538a158b62ed9906e3528f18
anamulamin <aadelbanat8991@gmail.com>
2025-01-17 16:39:51 +0600

    modifyed city area

--- src/components/dashboard/ManageCourier/ViewAllArea.jsx
+++ src/components/dashboard/ManageCourier/ViewAllArea.jsx
@@ -1,8 +1,8 @@
-
 import { useState, useEffect, useCallback } from "react";
 
 import useAxiosSecure from "../../../Hook/useAxiosSecure";
 import { Link, useNavigate, useParams } from "react-router-dom";
+import Swal from "sweetalert2";
 
 export default function ViewAllArea() {
   const [currentPage, setCurrentPage] = useState(0);
@@ -11,12 +11,11 @@ export default function ViewAllArea() {
   const [sortConfig, setSortConfig] = useState({ key: "id", direction: "asc" });
   const axiosSecure = useAxiosSecure();
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-  const {zone_id} = useParams();
+  const { zone_id } = useParams();
 
-  console.log(zone_id, "city id")
-
+  console.log(zone_id, "city id");
 
   const sortedcourierCities = useCallback(() => {
     const sortedData = [...courierCities];
@@ -46,13 +45,9 @@ export default function ViewAllArea() {
     setSortConfig({ key, direction });
   };
 
-
-
   const handlePageClick = (pageNumber) => {
     setCurrentPage(pageNumber);
   };
-
-  
 
   useEffect(() => {
     const fetchCourierCities = async () => {
@@ -71,32 +66,68 @@ export default function ViewAllArea() {
     fetchCourierCities();
   }, [axiosSecure, zone_id]);
 
-  const goBack = ()=> {
-    navigate(-1)
-  }
+  const goBack = () => {
+    navigate(-1);
+  };
+
+  const handleSubmitAllZonesData = async (e) => {
+    e.preventDefault();
+    const formData = {
+      zone_id: zone_id,
+      items: courierCities,
+    };
+
+    try {
+      const res = await axiosSecure.post(`/areas/multiple`, formData);
+
+      if (res.status === 200 || res.status === 201) {
+        Swal.fire({
+          title: "Success!",
+          text: "Zones updated successfully",
+          icon: "success",
+          confirmButtonText: "Ok",
+        });
+      }
+    } catch (err) {
+      Swal.fire({
+        title: "Error!",
+        text: "Something went wrong!",
+        icon: "error",
+        confirmButtonText: "Ok",
+      });
+    }
+  };
 
   return (
     <div className="p-6 pt-0">
       <div className="breadcrumbs text-sm">
-  <ul>
-    <li><Link to={`/dashboard`}>Dashboard</Link></li>
-    <li><Link to={`/dashboard/view-all-cities`}>Cities</Link></li>
-    <li onClick={goBack} className="cursor-pointer">Zones</li>
-    <li>Area</li>
-  </ul>
-</div>
+        <ul>
+          <li>
+            <Link to={`/dashboard`}>Dashboard</Link>
+          </li>
+          <li>
+            <Link to={`/dashboard/view-all-cities`}>Cities</Link>
+          </li>
+          <li onClick={goBack} className="cursor-pointer">
+            Zones
+          </li>
+          <li>Area</li>
+        </ul>
+      </div>
+      <button
+        className="btn btn-sm btn-primary"
+        onClick={handleSubmitAllZonesData}
+      >
+        Submit All Area Data
+      </button>
       <div className="">
         <h1 className="text-3xl header font-semibold mb-9">Area List</h1>
 
         <table className="min-w-full shadow table-auto border-collapse">
           <thead className="text-lg">
             <tr>
-              <td
-                className="border flex justify-center items-center gap-2 text-lg p-2 text-center cursor-pointer"
-                
-              >
+              <td className="border flex justify-center items-center gap-2 text-lg p-2 text-center cursor-pointer">
                 SL{" "}
-                
               </td>
               <td
                 className="border p-2 text-center cursor-pointer"
@@ -106,7 +137,7 @@ export default function ViewAllArea() {
                 {sortConfig.key === "zone_name" &&
                   (sortConfig.direction === "asc" ? "🔼" : "🔽")}
               </td>
-              
+
               <td
                 className="border p-2 text-center cursor-pointer"
                 onClick={() => handleSort("zone_id")}
@@ -115,24 +146,19 @@ export default function ViewAllArea() {
                 {sortConfig.key === "zone_id" &&
                   (sortConfig.direction === "asc" ? "🔼" : "🔽")}
               </td>
-              
             </tr>
           </thead>
           <tbody>
-            {paginatedData()?.length > 0 && paginatedData().map((item, index) => (
-              <tr key={item.id} className="border-b text-center">
-                <td className="border p-2 ">
-                  {index + 1 + currentPage * itemsPerPage}
-                </td>
-                <td className="border p-2">{item.area_name}</td>
-                <td className="border p-2">{item.area_id}</td>
-
-
-               
-               
-                
-              </tr>
-            ))}
+            {paginatedData()?.length > 0 &&
+              paginatedData().map((item, index) => (
+                <tr key={item.id} className="border-b text-center">
+                  <td className="border p-2 ">
+                    {index + 1 + currentPage * itemsPerPage}
+                  </td>
+                  <td className="border p-2">{item.area_name}</td>
+                  <td className="border p-2">{item.area_id}</td>
+                </tr>
+              ))}
           </tbody>
         </table>
 
@@ -144,17 +170,17 @@ export default function ViewAllArea() {
             <button
               key={pageIndex}
               onClick={() => handlePageClick(pageIndex)}
-              className={`px-3 py-1 border rounded-md ${currentPage === pageIndex
-                ? "bg-blue-500 text-white"
-                : "hover:bg-gray-200"
-                }`}
+              className={`px-3 py-1 border rounded-md ${
+                currentPage === pageIndex
+                  ? "bg-blue-500 text-white"
+                  : "hover:bg-gray-200"
+              }`}
             >
               {pageIndex + 1}
             </button>
           ))}
         </div>
       </div>
-     
     </div>
   );
 }
