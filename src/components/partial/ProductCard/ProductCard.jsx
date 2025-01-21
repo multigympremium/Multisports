5aaab78761c108fd2600bce9ca7a3ab07b73a104
anamulamin <aadelbanat8991@gmail.com>
2025-01-21 10:02:37 +0600

    modifyed product card area

--- src/components/partial/ProductCard/ProductCard.jsx
+++ src/components/partial/ProductCard/ProductCard.jsx
@@ -43,101 +43,104 @@ const ProductCard = ({
   };
 
   return (
-    <div
-      key={product._id}
-      className={`bg-white relative md:hover:scale-105 cursor-pointer overflow-hidden rounded-lg md:hover:shadow-lg transition-transform duration-300 ${
-        border && "border"
-      }`}
-    >
-      {/* Badges */}
-      {renderBadge(
-        showDiscount && product.discount > 0,
-        `${product.discount}% OFF`,
-        "bg-gray-800 top-2 md:top-5 md:left-5"
-      )}
-      {renderBadge(
-        showNewArrival,
-        "New Arrival",
-        "bg-[#923670] top-7 md:top-12 md:left-5"
-      )}
-      {renderBadge(
-        isPopular,
-        "Popular now",
-        "bg-blue-500 top-7 md:top-12 md:left-5"
-      )}
-      {renderBadge(
-        isSpecial,
-        "Special",
-        "bg-blue-500 top-7 md:top-12 md:left-5"
-      )}
-
-      {/* Product Image */}
-      <img
-        src={`https://mgpwebaps.s3.eu-north-1.amazonaws.com/multi-sports/${product.thumbnail}`}
-        alt={product.productTitle}
-        className="h-[170px] md:h-[340px] w-full rounded object-cover transition-transform duration-300"
-      />
+    <div className="relative">
       <WishlistIcon
         item={product}
         setIsDeleted={setIsDeleted}
         setIsEdited={setIsEdited}
       />
+      <div
+        key={product._id}
+        className={`bg-white  md:hover:scale-105 cursor-pointer overflow-hidden rounded-lg md:hover:shadow-lg transition-transform duration-300 ${
+          border && "border"
+        }`}
+        onClick={handleClick}
+      >
+        {/* Badges */}
+        {renderBadge(
+          showDiscount && product.discount > 0,
+          `${product.discount}% OFF`,
+          "bg-gray-800 top-2 md:top-5 md:left-5"
+        )}
+        {renderBadge(
+          showNewArrival,
+          "New Arrival",
+          "bg-[#923670] top-7 md:top-12 md:left-5"
+        )}
+        {renderBadge(
+          isPopular,
+          "Popular now",
+          "bg-blue-500 top-7 md:top-12 md:left-5"
+        )}
+        {renderBadge(
+          isSpecial,
+          "Special",
+          "bg-blue-500 top-7 md:top-12 md:left-5"
+        )}
 
-      {/* Product Details */}
-      <div className="p-4" onClick={handleClick}>
-        <h3 className="text-sm md:text-base font-semibold mb-1 block md:hidden">
-          {product.productTitle?.length > 12
-            ? `${product.productTitle.slice(0, 12)}..`
-            : product.productTitle}
-        </h3>
-        <h3 className="text-base font-semibold mb-2 hidden md:block">
-          {product.productTitle}
-        </h3>
-
-        {/* Description */}
-        <p className="text-gray-500 mb-1 text-xs lg:hidden">
-          <div
-            dangerouslySetInnerHTML={{
-              __html: `${product.fullDescription.slice(0, 20)}...`,
-            }}
-          />
-        </p>
-        <p className="text-gray-500 mb-2 hidden lg:block">
-          <div
-            dangerouslySetInnerHTML={{
-              __html: `${product.fullDescription.slice(0, 40)}...`,
-            }}
-          />
-        </p>
-
-        {/* Price */}
-        <div className="flex flex-col-reverse">
-          <p className="text-xs md:text-base line-through opacity-60">
-            BDT {product.price}.00
-          </p>
-          {product.discount > 0 ? (
-            <p className="text-sm md:text-lg font-semibold">
-              BDT {getDiscountedPrice()}
-            </p>
-          ) : (
-            <p className="opacity-0">3</p>
-          )}
-        </div>
-
-        {/* Modal */}
-        <Modal
-          id={`modal_${product.productTitle.replace(/\s+/g, "_")}`}
-          object_id={product._id}
-          title={product.productTitle}
-          sizes={product.productSizeValue}
-          image={`https://mgpwebaps.s3.eu-north-1.amazonaws.com/multi-sports/${product.thumbnail}`}
-          description={product.fullDescription}
-          colors={product.colorAndSize}
-          price={product.price}
-          setIsShowModal={setIsShowModal}
-          isShowModal={isShowModal}
-          product={product}
+        {/* Product Image */}
+        <img
+          src={`https://mgpwebaps.s3.eu-north-1.amazonaws.com/multi-sports/${product.thumbnail}`}
+          alt={product.productTitle}
+          className="h-[170px] md:h-[340px] w-full rounded object-cover transition-transform duration-300"
         />
+
+        {/* Product Details */}
+        <div className="p-4">
+          <h3 className="text-sm md:text-base font-semibold mb-1 block md:hidden">
+            {product.productTitle?.length > 12
+              ? `${product.productTitle.slice(0, 12)}..`
+              : product.productTitle}
+          </h3>
+          <h3 className="text-base font-semibold mb-2 hidden md:block">
+            {product.productTitle}
+          </h3>
+
+          {/* Description */}
+          <p className="text-gray-500 mb-1 text-xs lg:hidden">
+            <div
+              dangerouslySetInnerHTML={{
+                __html: `${product.fullDescription.slice(0, 20)}...`,
+              }}
+            />
+          </p>
+          <p className="text-gray-500 mb-2 hidden lg:block">
+            <div
+              dangerouslySetInnerHTML={{
+                __html: `${product.fullDescription.slice(0, 40)}...`,
+              }}
+            />
+          </p>
+
+          {/* Price */}
+          <div className="flex flex-col-reverse">
+            <p className="text-xs md:text-base line-through opacity-60">
+              BDT {product.price}.00
+            </p>
+            {product.discount > 0 ? (
+              <p className="text-sm md:text-lg font-semibold">
+                BDT {getDiscountedPrice()}
+              </p>
+            ) : (
+              <p className="opacity-0">3</p>
+            )}
+          </div>
+
+          {/* Modal */}
+          <Modal
+            id={`modal_${product.productTitle.replace(/\s+/g, "_")}`}
+            object_id={product._id}
+            title={product.productTitle}
+            sizes={product.productSizeValue}
+            image={`https://mgpwebaps.s3.eu-north-1.amazonaws.com/multi-sports/${product.thumbnail}`}
+            description={product.fullDescription}
+            colors={product.colorAndSize}
+            price={product.price}
+            setIsShowModal={setIsShowModal}
+            isShowModal={isShowModal}
+            product={product}
+          />
+        </div>
       </div>
     </div>
   );
