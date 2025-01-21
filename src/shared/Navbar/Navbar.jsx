9a6934d5f34bee01a9b0939366e454d9989edc5e
anamulamin <aadelbanat8991@gmail.com>
2025-01-21 21:22:30 +0600

    modifyed responsive issus of sidebar and searchbar

--- src/shared/Navbar/Navbar.jsx
+++ src/shared/Navbar/Navbar.jsx
@@ -148,13 +148,16 @@ const Navbar = () => {
                 <div role="button" className="">
                   <div className="drawer z-10">
                     <input
-                      id="my-drawer"
+                      id="sidebar-drawer"
                       type="checkbox"
                       className="drawer-toggle"
                     />
                     <div className="drawer-content">
                       {/* Page content here */}
-                      <label htmlFor="my-drawer" className=" drawer-button">
+                      <label
+                        htmlFor="sidebar-drawer"
+                        className=" drawer-button"
+                      >
                         <MdMenu
                           onClick={() => {
                             toggleSidebar();
@@ -165,7 +168,7 @@ const Navbar = () => {
                     </div>
                     <div className="drawer-side">
                       <label
-                        htmlFor="my-drawer"
+                        htmlFor="sidebar-drawer"
                         aria-label="close sidebar"
                         className="drawer-overlay"
                       ></label>
@@ -262,7 +265,7 @@ const Navbar = () => {
               {user ? (
                 <details className="dropdown">
                   <summary className="avatar w-16 m-1">
-                    <div className="bg-neutral text-neutral-content w-16 rounded-full !flex justify-center items-center mx-auto">
+                    <div className="bg-neutral text-neutral-content w-12 md:w-16 rounded-full !flex justify-center items-center mx-auto">
                       {user?.photourl ? (
                         <CustomImage imageKey={user?.photourl} />
                       ) : (
